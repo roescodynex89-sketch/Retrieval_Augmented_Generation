176478d38a3937410d164110db367060a93f5fd1
roescodynex89-sketch <roescody.nex89@gmail.com>
2026-09-25 18:26:29 +0600

    output

--- main.py
+++ main.py
@@ -98,3 +98,34 @@ print("\nRetrieved Chunks:")
 for i, document in enumerate(results["documents"][0]):
     print(f"\nResult {i + 1}:")
     print(document)
+
+    
+
+
+
+
+
+# expect  output>>>>
+
+# Document loaded successfully!
+
+# Chunks:
+# Chunk 0:
+# Bitcoin is a decentralized digital asset...
+
+# Embedding created!
+# Number of chunks: ...
+# Embedding size: 384
+
+# Data stored in Chroma!
+
+# User Question:
+# How does liquidity affect Bitcoin?
+
+# Retrieved Chunks:
+# Result 1:
+# ...
+# Result 2:
+# ...
+# Result 3:
+# ...
